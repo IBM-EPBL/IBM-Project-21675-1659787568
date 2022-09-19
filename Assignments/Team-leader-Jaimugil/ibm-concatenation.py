017c06a6836202c98cf89d3221be779486601cb6
JaimugilC <megalajai001@gmail.com>
2022-09-19 14:57:26 +0530

    Assignments

---
+++ Assignments/Team-leader-Jaimugil/ibm-concatenation.py
@@ -0,0 +1,8 @@
+
+s1=input('Enter the first string : ')
+s2=input('Ente the second string ')
+s3=s1+s2
+print('The concatenated string:'+s3)
+print('sliced string :'+s3[1:len(s3):2])
+print('reversing the string :'+s3[::-1])
+
